927cebbccc2d3b5616680ed358e432e27029259d
FreetyIndrianiSafitri <171821nov>
2021-10-08 18:53:29 -0800

    Menyelesaikan video ke 4 tentang Blade Templating Engine pada Laravel 8

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,72 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Freety Indriani Safitri",
-        "email" => "freetyindriani03@gmail.com",
+        "email" => "193040175.freety@mail.unpas.ac.id",
         "image" => "profile.png"
     ]);
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Freety Indriani Safitri",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque libero earum debitis officiis, est sapiente, nemo aspernatur tempora hic perspiciatis ducimus, culpa expedita necessitatibus quibusdam architecto! Libero asperiores neque temporibus dolore beatae autem optio modi recusandae dolorum minima at deleniti mollitia omnis facilis natus, impedit culpa. Necessitatibus non modi repudiandae nesciunt, esse sapiente magnam a at magni accusantium, consequatur, sed officiis libero eum alias. Dolor accusantium, cupiditate libero consectetur facere unde mollitia, illum quam omnis tempore nisi iste dolorem molestias."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Lady",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et unde velit asperiores eligendi? Delectus, vel. Dolor repudiandae veritatis ipsam sit eaque perferendis officiis harum, porro repellat, eveniet fuga odit ratione provident sequi assumenda quasi nihil. Dolores architecto, labore cupiditate consectetur exercitationem iure nemo? Ex repellat accusamus libero, culpa quae nihil reprehenderit. Voluptatem ab a ipsam porro, adipisci fuga ducimus quod expedita accusamus ea similique enim maiores nobis nihil distinctio tempore. Ea debitis hic harum assumenda nostrum, esse sapiente aut saepe corporis atque accusantium iure dolorem amet odio earum quisquam optio laboriosam illo perferendis dignissimos itaque sequi iste recusandae! Doloribus!"
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
 });
+
+// halaman single post  
+Route::get('posts/{slug}', function ($slug) {
+
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Freety Indriani Safitri",
+            "body" => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque libero earum debitis officiis, est sapiente, nemo aspernatur tempora hic perspiciatis ducimus, culpa expedita necessitatibus quibusdam architecto! Libero asperiores neque temporibus dolore beatae autem optio modi recusandae dolorum minima at deleniti mollitia omnis facilis natus, impedit culpa. Necessitatibus non modi repudiandae nesciunt, esse sapiente magnam a at magni accusantium, consequatur, sed officiis libero eum alias. Dolor accusantium, cupiditate libero consectetur facere unde mollitia, illum quam omnis tempore nisi iste dolorem molestias."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Lady",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et unde velit asperiores eligendi? Delectus, vel. Dolor repudiandae veritatis ipsam sit eaque perferendis officiis harum, porro repellat, eveniet fuga odit ratione provident sequi assumenda quasi nihil. Dolores architecto, labore cupiditate consectetur exercitationem iure nemo? Ex repellat accusamus libero, culpa quae nihil reprehenderit. Voluptatem ab a ipsam porro, adipisci fuga ducimus quod expedita accusamus ea similique enim maiores nobis nihil distinctio tempore. Ea debitis hic harum assumenda nostrum, esse sapiente aut saepe corporis atque accusantium iure dolorem amet odio earum quisquam optio laboriosam illo perferendis dignissimos itaque sequi iste recusandae! Doloribus!"
+        ],
+    ];
+
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] == $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
+}); 
+

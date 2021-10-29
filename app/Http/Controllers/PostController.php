8badee984cd36c123cf75980ced58b51faf8323d
FreetyIndrianiSafitri <171821nov>
2021-10-28 23:37:02 -0800

    Mempelajari mengenai Searching dan Pagination pada laravel 8 #video 13

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,16 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Category;
+use App\Models\User;
 
 class PostController extends Controller
 {
     public function index()
     {
+
+        $title = '';
+
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' in ' . $author->name;
+        }
+
         return view('posts', [
             "title" => "All Posts",
             "active" => "posts",
             // *post* => POst::all()
-            "posts" => Post::latest()->get()
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
+
         ]);
     }
 
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">{{ $title }}</h1>
+    <h1 class="mb-3 text-center">{{ $title }}</h1>
+
+    <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+            <form action="/blog">
+                @if (request('category'))
+                    <input type="hidden" name="category" value="{{ request('category') }}">
+                @endif
+                 @if (request('author'))
+                    <input type="hidden" name="author" value="{{ request('author') }}">
+                @endif
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search.." name="search" value="{{ request('search') }}">
+                    <button class="btn btn-danger" type="submit" >Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
 
 @if($posts->count())
 <div class="card mb-3">
@@ -10,7 +27,7 @@
             <h5 class="card-title"><a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a></h5>
             <p>
                 <small class="text-muted"> 
-                    By. <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none"> {{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
+                    By. <a href="/blog?author={{ $posts[0]->author->username }}" class="text-decoration-none">{{ $posts[0]->author->name }}</a> in <a href="/blog?category={{ $posts[0]->category->slug }}" class="text-decoration-none"> {{ $posts[0]->category->name }}</a> {{ $posts[0]->created_at->diffForHumans() }}
                 </small>        
             </p>
                     <p class="card-text">{{ $posts[0]->excerpt }}</p>
@@ -19,11 +36,7 @@
 
         </div>
     </div>
-@else
 
-<p class="text-center fs-4">No Post found.</p>
-
-@endif
 
 
 <div class="container">
@@ -31,13 +44,13 @@
             @foreach ($posts->skip(1) as $post)
             <div class="col-md-4 mb-4">
                 <div class="card">
-                    <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/categories/{{ $posts[0]->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
+                    <div class="position-absolute px-3 py-2" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/blog?category={{ $posts[0]->category->slug }}" class="text-white text-decoration-none">{{ $post->category->name }}</a></div>
                     <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
                     <div class="card-body">
                         <h5 class="card-title">{{ $post->title }}</h5>
                          <p>
                             <small class="text-muted"> 
-                                By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
+                                By. <a href="/blog?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> {{ $post->created_at->diffForHumans() }}
                             </small>        
                         </p>
                         <p class="card-text">{{ $post->excerpt }}</p>
@@ -48,5 +61,15 @@
             @endforeach
         </div>
     </div>
+
+@else
+
+<p class="text-center fs-4">No Post found.</p>
+
+@endif
+
+<div class="d-flex justify-content-end">
+        	{{ $posts->links() }}
+</div>
 
 @endsection

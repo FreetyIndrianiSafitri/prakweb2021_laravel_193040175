436489d113c5d53ca07ed146ec9947dfb9c328d1
FreetyIndrianiSafitri <171821nov>
2021-10-29 16:27:33 -0800

    Memperbaiki user regisration dan memperbaiki halaman login pada laravel 8 #video 15

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,31 +1,33 @@
 @extends('layouts.main')
 
 @section('container')
-
+    
 <div class="row justify-content-center">
-    <div class="col-lg-5">
-        <main class="form-registration">
-            <h1 class="h3 mb-3 fw-normal text-center mt-5">Registration Form</h1>
+    <div class="col-md-4">
+        @if (session()->has('success'))
+            
+         <div class="alert alert-success alert-dismissible fade show" role="alert">
+             {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+
+        @endif
+
+        
+        <main class="form-signin">
+            <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
             <form>
                 <div class="form-floating">
-                    <input type="text" name="name" class="form-control rounded-top" id="name" placeholder="Name">
-                    <label for="name">Name</label>
+                    <input type="email" class="form-control mb-3 mt-3" id="floatingInput" placeholder="name@example.com">
+                    <label for="floatingInput">Email address</label>
                 </div>
                 <div class="form-floating">
-                    <input type="text" name="username" class="form-control" id="username" placeholder="Username">
-                    <label for="username">Username</label>
+                    <input type="password" class="form-control mb-3 mt-3" id="floatingPassword" placeholder="Password">
+                    <label for="floatingPassword">Password</label>
                 </div>
-                <div class="form-floating">
-                    <input type="email" name="email" class="form-control" id="email" placeholder="name@example.com">
-                    <label for="email">Email address</label>
-                </div>
-                <div class="form-floating">
-                    <input type="password" name="password" class="form-control rounded-bottom" id="password" placeholder="Password">
-                    <label for="password">Password</label>
-                </div>
-                <button class="w-100 btn btn-lg btn-danger mt-3" type="submit">Register</button>
+                <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
             </form>
-            <small class="d-block text-center">Already registered?<a href="/login">Login</a></small>
+            <small class="d-block text-center mt-3">Not registered? <a href="/register">Register Now!</a></small>
         </main>
     </div>
 </div>

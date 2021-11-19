096cc32ab085509d084732c5fc7b6000f26092af
FreetyIndrianiSafitri <freetyindrianis03@gmail.com>
2021-11-19 23:14:26 +0700

    Membuat tampilan dashboard UI pada laravel 8 #video 17

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -55,8 +55,11 @@ Route::get('/login', [LoginController::class, 'index'])->name('login')->middlewa
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::post('/logout', [LoginController::class, 'logout']);
 Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
 Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
 
 // Route::get('/categories/{category:slug}', function(Category $category) {
 //     return view('posts', [
